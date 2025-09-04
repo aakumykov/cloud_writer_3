ee1a8764f701d56a6287dd50b4d35975894a3e19
Andrey Kumykov <aakumykov@yandex.ru>
2025-09-04 17:38:00 +0300

    Подправил тест LocalCreateOneLevelDir, где задействовал метод с двумя аргументами.

--- local/src/androidTest/java/com/github/aakumykov/local_cloud_writer_3/create_dir/LocalCreateOneLevelDir.kt
+++ local/src/androidTest/java/com/github/aakumykov/local_cloud_writer_3/create_dir/LocalCreateOneLevelDir.kt
@@ -77,119 +77,16 @@ class LocalCreateOneLevelDir : LocalBase() {
     fun deep_dir_is_created_step_by_step_in_deep() = runBlocking {
         val parentDirName = randomId
         val childDirName = randomId
+
         val deepDirName = CloudWriter.mergeFilePaths(parentDirName, childDirName)
         val expectedDirPath = cloudWriter.absolutePathFor(deepDirName)
+
         cloudWriter.createOneLevelDir(parentDirName)
+
         Assert.assertEquals(
             expectedDirPath,
-            cloudWriter.createOneLevelDir(deepDirName)
+            cloudWriter.createOneLevelDir(parentDirName, childDirName)
         )
     }
 
-
-    /*@Test
-    fun virtual_root_subdir_created(): Unit = runBlocking {
-        val dirPath = CloudWriter.mergeFilePaths(storageRootPath, randomId)
-        Assert.assertEquals(
-            dirPath,
-            cloudWriter.createOneLevelDir(dirPath)
-        )
-    }
-
-
-    @Test
-    fun downloads_dir_subdir_created(): Unit = runBlocking {
-        val dirAbsolutePath = CloudWriter.mergeFilePaths(downloadsDirPath, randomId)
-        Assert.assertEquals(
-            dirAbsolutePath,
-            cloudWriter.createOneLevelDir(dirAbsolutePath)
-        )
-    }
-
-
-    @Test
-    fun music_dir_subdir_created(): Unit = runBlocking {
-        val dirAbsolutePath = CloudWriter.mergeFilePaths(musicDirPath, randomId)
-        Assert.assertEquals(
-            dirAbsolutePath,
-            cloudWriter.createOneLevelDir(dirAbsolutePath)
-        )
-    }
-
-
-    @Test
-    fun multi_level_dir_throws_exception() {
-        Assert.assertThrows(CloudWriterException::class.java) {
-            val dirName = CloudWriter.mergeFilePaths(randomId, randomId)
-            runBlocking {
-                cloudWriter.createOneLevelDir(
-                    dirName = CloudWriter.mergeFilePaths(storageRootPath, dirName)
-                )
-            }
-        }
-    }
-
-
-    //
-    // Относительныя методы
-    //
-
-    @Test
-    fun relative_root_dir_throws_exception() {
-        Assert.assertThrows(CloudWriterException::class.java) {
-            runBlocking {
-                cloudWriter.createOneLevelDir(ROOT_DIR)
-            }
-        }
-    }
-
-
-    @Test
-    fun relative_empty_dir_throws_exception() {
-        Assert.assertThrows(CloudWriterException::class.java) {
-            runBlocking {
-                cloudWriter.createOneLevelDir(EMPTY_DIR)
-            }
-        }
-    }
-
-
-    @Test
-    fun relative_one_level_dir_created() = runBlocking {
-        val dirName = randomId
-        Assert.assertEquals(
-            CloudWriter.mergeFilePaths(storageRootPath, dirName),
-            cloudWriter.createOneLevelDir(dirName)
-        )
-    }
-
-
-    @Test
-    fun relative_standard_dir_throws_exception() {
-        Assert.assertThrows(CloudWriterException::class.java) {
-            runBlocking {
-                cloudWriter.createOneLevelDir(android.os.Environment.DIRECTORY_DOWNLOADS)
-            }
-        }
-    }
-
-
-    @Test
-    fun relative_illegal_name_dir_throws_exception() {
-        Assert.assertThrows(CloudWriterException::class.java) {
-            runBlocking {
-                cloudWriter.createOneLevelDir(ILLEGAL_DIR_NAME)
-            }
-        }
-    }
-
-
-    @Test
-    fun relative_multi_level_dir_throws_exception() {
-        Assert.assertThrows(CloudWriterException::class.java) {
-            runBlocking {
-                cloudWriter.createOneLevelDir(CloudWriter.mergeFilePaths(randomId, randomId))
-            }
-        }
-    }*/
 }
